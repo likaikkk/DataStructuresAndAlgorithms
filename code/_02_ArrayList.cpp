bd488d0d130806f15f8abae96e7d88f92e128af8
lewis <30796729876@qq.com>
2023-08-21 23:50:57 +0800

    完成了顺序表的实现

---
+++ code/_02_ArrayList.cpp
@@ -0,0 +1,193 @@
+#include<iostream>
+
+using namespace std;
+
+template<class T>       //线性表的元素类型为T
+class ArrayList         //定义顺序表ArrayList
+{
+private:
+    T* arrayList;       //存储顺序表的实例
+    int maxSize;
+    int curLen;
+    //int position;
+public:
+    ArrayList(const int size) {
+        if (size < 1) {
+            cout << "Size must be greater than 0";
+            throw - 1;
+        }
+        maxSize = size;
+        arrayList = new T[maxSize];
+        curLen = 0;
+        //position = 0;
+    }
+    ~ArrayList() {
+        delete[] arrayList;
+    }
+    void clear() {
+        delete[]arrayList;
+        curLen = 0;
+        //position = 0;
+        arrayList = new T[maxSize];
+    }
+    int Length();
+    bool Append(const T value);
+    bool Insert(const int p, const T value);
+    bool Delete(const int p);
+    bool GetValue(const int p, T& value);
+    bool SetValue(const int p, const T value);
+    bool GetPos(int& p, const T value);
+    int Search(T x);
+};
+
+template<class T>
+int ArrayList<T>::Length()
+{
+    return curLen;
+}
+
+template<class T>
+bool ArrayList<T>::Append(const T value)
+{
+    if (curLen >= maxSize) {
+        cout << "The List is overflow" << endl;
+        return false;
+    }
+    arrayList[curLen] = value;
+    curLen++;
+    return true;
+}
+
+template<class T>
+bool ArrayList<T>::Insert(const int p, const T value)
+{
+    if (curLen >= maxSize) {
+        cout << "The List is overflow" << endl;
+        return false;
+    }
+    if (p<0 || p>curLen) {
+        cout << "Insertion point is illegal" << endl;
+        return false;
+    }
+    for (int i = curLen;i > p;i--) {
+        arrayList[i] = arrayList[i - 1];
+    }
+    arrayList[p] = value;
+    curLen++;
+    return true;
+}
+
+template<class T>
+bool ArrayList<T>::Delete(const int p)
+{
+    if (curLen <= 0) {
+        cout << "No element to delete" << endl;
+        return false;
+    }
+    if (p<0 || p>curLen) {
+        cout << "Deletion is illegal" << endl;
+        return false;
+    }
+    for (int i = p;i < curLen - 1;i++) {
+        arrayList[i] = arrayList[i + 1];
+    }
+    curLen--;
+
+    return true;
+}
+
+template<class T>
+bool ArrayList<T>::GetValue(const int p, T& value)
+{
+    if (p<0 || p>curLen) {
+        cout << "Position is illegal" << endl;
+        return false;
+    }
+    value = arrayList[p];
+    return true;
+}
+
+template<class T>
+bool ArrayList<T>::SetValue(const int p, const T value)
+{
+    if (p<0 || p>curLen) {
+        cout << "Position is illegal" << endl;
+        return false;
+    }
+    arrayList[p] = value;
+    return true;
+}
+
+template<class T>
+bool ArrayList<T>::GetPos(int& p, const T value)
+{
+    for (int i = 0;i < curLen;i++) {
+        if (arrayList[i] == value)
+        {
+            p = i + 1;
+            return true;
+        }
+    }
+    return false;
+}
+
+template<class T>
+int ArrayList<T>::Search(T x)
+{
+    for (int i = 0;i < curLen;i++)
+        if (arrayList[i] == x)
+            return i;
+    return -1;
+}
+
+int main() {
+    ArrayList<int> arr(5);
+
+    cout << "Appending values:" << endl;
+    for (int i = 0; i < 5; i++) {
+        arr.Append(i * 2);
+    }
+
+    cout << "Length of the list: " << arr.Length() << endl;
+
+    cout << "Inserting value 10 at position 2:" << endl;
+    arr.Insert(2, 10);
+
+    cout << "List after insertion:" << endl;
+    for (int i = 0; i < arr.Length(); i++) {
+        int value;
+        arr.GetValue(i, value);
+        cout << value << " ";
+    }
+    cout << endl;
+
+    cout << "Deleting value at position 3:" << endl;
+    arr.Delete(3);
+
+    cout << "List after deletion:" << endl;
+    for (int i = 0; i < arr.Length(); i++) {
+        int value;
+        arr.GetValue(i, value);
+        cout << value << " ";
+    }
+    cout << endl;
+
+    int pos;
+    bool found = arr.GetPos(pos, 6);
+    if (found) {
+        cout << "Value 6 found at position: " << pos << endl;
+    }
+    else {
+        cout << "Value 6 not found in the list" << endl;
+    }
+
+    int index = arr.Search(8);
+    if (index != -1) {
+        cout << "Value 8 found at index: " << index << endl;
+    }
+    else {
+        cout << "Value 8 not found in the list" << endl;
+    }
+
+    return 0;
+}
--- notes/_01_LinearList.md
+++ notes/_01_LinearList.md
@@ -33,7 +33,7 @@
 > };
 > ```
 >
-> *关于template class见code中的1_Template-Class.cpp;*
+> *关于template class见[_1_Template-Class.cpp](E:\Programming\Notes\DataStructuresAndAlgorithms\code\_01_Template-Class.cpp)
 
 #### 1.3线性表的主要操作
 
@@ -43,7 +43,7 @@
 > 2. 取线性表的第i个元素的值：`DataType getValue(List& L, int i)`
 > 3. 计算线性表的长度：`int Length(List& L)`
 > 4. 线性表的查找：`position Search(List& L, DataType x)`
-> 5. 线性表的插入：`int Insertd (List& L, int i, DataType x)`
+> 5. 线性表的插入：`int Insert (List& L, int i, DataType x)`
 > 6. 线性表的删除：`int Remove(List& L, int i, DataType& x)`
 > 7. 线性表的遍历：`void Traverse(List& L)`
 > 8. 线性表的排序：`void Sort(List& L)`
@@ -53,9 +53,21 @@
 #### 1.4线性表的存储结构
 
 > 1. **定长的顺序存储结构**，简称`顺序表`，程序中通过创建数组来建立这种存储结构，特点是**线性表元素被分配到一块连续的存储空间，元素顺序存储在这些连续的空间中，数据元素之间是`“物理位置相邻”`的**。定长的顺序存储结构限定了线性表长度的变化不得超过该固定长度，这是顺序表的不足之处。
+>
 > 2. **变长的线性存储结构**，简称`链表`，链接式存储结构使用指针，按照线性表的前驱和后继关系将各元素用指针链接起来。变长的线性存储结构对线性表的长度不加限制。
 
-## 2. 顺序表
+## 2. 顺序表  
+
+把采用顺序存储结构的线性表简称为**顺序表**，也称为**向量**，有以下主要特征：
+
+1. 元素的数据类型**相同**。
+2. 元素顺序的存储在一片**地址连续的存储空间**中，一个元素按照存储顺序有唯一的索引值，又称为下标，可以随机存取表中的元素。
+3. 逻辑关系相邻的两个元素在物理位置上也相邻。
+4. 在程序中，数组变量说明语句一般使用常数作为向量长度，长度是静态常熟，在程序执行时不变。
+
+#### 2.1顺序表的实现
+
+> *见* [_02_ArrayList.cpp](E:\Programming\Notes\DataStructuresAndAlgorithms\code\_02_ArrayList.cpp )
 
 ## 3. 链表
 
