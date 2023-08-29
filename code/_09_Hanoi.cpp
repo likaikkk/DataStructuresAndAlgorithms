00aa0b05c5137f9d82131922a484cd2307256a2d
lewis <3079672976@qq.com>
2023-08-29 21:50:46 +0800

    提交汉诺塔问题代码

---
+++ code/_09_Hanoi.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+
+void Hanoi(int n, char A, char B, char C) {
+    if (n == 1) {//only one disk
+        cout << "Move disk from " << A << " to " << C << endl;
+    }
+    else {
+        Hanoi(n - 1, A, C, B);//First move n-1 disks from pole A to pole B with the aid of pole B
+        cout << "Move disk from " << A << " to " << C << endl;//Move the remaining disk from pole A to pole C
+        Hanoi(n - 1, B, A, C);//Finally move the n-1 disks preciously moved to the B to the pole C with the aid of pole A
+    }
+}
+
+int main() {
+    int n;
+    cout << "Enter the number of disks: ";
+    cin >> n;
+
+    Hanoi(n, 'A', 'B', 'C');
+
+    return 0;
+}
