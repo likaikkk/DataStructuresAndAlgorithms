026dff579063efa930773125106a4210723fb6a7
lewis <3079672976@qq.com>
2023-08-29 21:53:28 +0800

    修改注释错误

--- code/_09_Hanoi.cpp
+++ code/_09_Hanoi.cpp
@@ -8,7 +8,7 @@ void Hanoi(int n, char A, char B, char C) {
     else {
         Hanoi(n - 1, A, C, B);//First move n-1 disks from pole A to pole B with the aid of pole B
         cout << "Move disk from " << A << " to " << C << endl;//Move the remaining disk from pole A to pole C
-        Hanoi(n - 1, B, A, C);//Finally move the n-1 disks preciously moved to the B to the pole C with the aid of pole A
+        Hanoi(n - 1, B, A, C);//Finally move the n-1 disks previously moved to the B to the pole C with the aid of pole A
     }
 }
 
